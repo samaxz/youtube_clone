93d0d55e28957cf9f64e2af60d63a64b8ab22c23
samax <samax.zer@gmail.com>
2024-02-10 17:23:00 +0300

    refactored searched_items_notifier.dart and made it non-disposable

--- lib/logic/notifiers/searched_items_notifier.dart
+++ lib/logic/notifiers/searched_items_notifier.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
+import 'dart:developer';
+
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:youtube_clone/data/info/base_info.dart';
 import 'package:youtube_clone/data/info/base_info_state.dart';
@@ -15,62 +17,66 @@ mixin Item {
   String get id;
 }
 
-@riverpod
+// @riverpod
+// this solves the problem of results reloading after popping back
+// to the searched items list
+@Riverpod(keepAlive: true)
 class SearchItemsNotifier extends _$SearchItemsNotifier {
   @override
-  Map<int, List<BaseInfoState<Item>>> build() {
-    return {
-      0: [
-        const BaseInfoLoading(),
-      ],
-      1: [
-        const BaseInfoLoading(),
-      ],
-      3: [
-        const BaseInfoLoading(),
-      ],
-      4: [
-        const BaseInfoLoading(),
-      ],
-    };
+  List<BaseInfoState<Item>> build(int screenIndex) {
+    return [
+      const BaseInfoLoading(),
+    ];
   }
 
   Future<void> searchItems({
     required String query,
-    required int index,
+    required int screenIndex,
+    // bool isReloading = false,
+    bool added = false,
   }) async {
+    // log('SearchItemsNotifier state after searchItems($query, $screenIndex): $state');
+
+    // TODO uncomment this
     final service = ref.watch(youtubeServiceP);
     final itemsOrFailure = await service.searchItems(
       query,
-      pageToken: state[index]!.last.baseInfo.nextPageToken,
+      pageToken: state.last.baseInfo.nextPageToken,
     );
 
     state = itemsOrFailure.fold(
-      (l) => {
+      (l) => [
         ...state,
-        index: [
-          ...state[index]!,
-        ]..last = BaseInfoError(
-            baseInfo: state[index]!.last.baseInfo,
-            failure: l,
-          ),
-      },
-      (r) => {
+        BaseInfoError(
+          baseInfo: state.last.baseInfo,
+          failure: l,
+        ),
+      ],
+      (r) => [
         ...state,
-        index: [
-          ...state[index]!,
-        ]..last = BaseInfoLoaded(
-            BaseInfo(
-              data: [
-                ...state[index]!.last.baseInfo.data,
-                ...r.data,
-              ],
-              nextPageToken: r.nextPageToken,
-              totalPages: r.totalPages,
-              itemsPerPage: r.itemsPerPage,
-            ),
+        BaseInfoLoaded(
+          BaseInfo(
+            data: [
+              ...state.last.baseInfo.data,
+              ...r.data,
+            ],
+            nextPageToken: r.nextPageToken,
+            totalPages: r.totalPages,
+            itemsPerPage: r.itemsPerPage,
           ),
-      },
+        ),
+      ],
     );
+
+    log('SearchItemsNotifier state after searchItems($query, $screenIndex): $state');
+  }
+
+  void removeLast() {
+    // this is to prevent bad state inside searched items list's build()
+    if (state.length == 1) return;
+
+    state = List.from(state)..removeLast();
+
+    log('SearchItemsNotifier state after removeLast(): $state');
   }
 }
--- lib/logic/notifiers/searched_items_notifier.g.dart
+++ lib/logic/notifiers/searched_items_notifier.g.dart
@@ -7,22 +7,170 @@ part of 'searched_items_notifier.dart';
 // **************************************************************************
 
 String _$searchItemsNotifierHash() =>
-    r'9032df9b1684ca2663d59f0f1d60f22641b178f7';
+    r'1884b59c1ada107b7f83be104fff1a4342e09347';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+abstract class _$SearchItemsNotifier
+    extends BuildlessNotifier<List<BaseInfoState<Item>>> {
+  late final int screenIndex;
+
+  List<BaseInfoState<Item>> build(
+    int screenIndex,
+  );
+}
 
 /// See also [SearchItemsNotifier].
 @ProviderFor(SearchItemsNotifier)
-final searchItemsNotifierProvider = AutoDisposeNotifierProvider<
-    SearchItemsNotifier, Map<int, List<BaseInfoState<Item>>>>.internal(
-  SearchItemsNotifier.new,
-  name: r'searchItemsNotifierProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$searchItemsNotifierHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
+const searchItemsNotifierProvider = SearchItemsNotifierFamily();
 
-typedef _$SearchItemsNotifier
-    = AutoDisposeNotifier<Map<int, List<BaseInfoState<Item>>>>;
+/// See also [SearchItemsNotifier].
+class SearchItemsNotifierFamily extends Family<List<BaseInfoState<Item>>> {
+  /// See also [SearchItemsNotifier].
+  const SearchItemsNotifierFamily();
+
+  /// See also [SearchItemsNotifier].
+  SearchItemsNotifierProvider call(
+    int screenIndex,
+  ) {
+    return SearchItemsNotifierProvider(
+      screenIndex,
+    );
+  }
+
+  @override
+  SearchItemsNotifierProvider getProviderOverride(
+    covariant SearchItemsNotifierProvider provider,
+  ) {
+    return call(
+      provider.screenIndex,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'searchItemsNotifierProvider';
+}
+
+/// See also [SearchItemsNotifier].
+class SearchItemsNotifierProvider extends NotifierProviderImpl<
+    SearchItemsNotifier, List<BaseInfoState<Item>>> {
+  /// See also [SearchItemsNotifier].
+  SearchItemsNotifierProvider(
+    int screenIndex,
+  ) : this._internal(
+          () => SearchItemsNotifier()..screenIndex = screenIndex,
+          from: searchItemsNotifierProvider,
+          name: r'searchItemsNotifierProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$searchItemsNotifierHash,
+          dependencies: SearchItemsNotifierFamily._dependencies,
+          allTransitiveDependencies:
+              SearchItemsNotifierFamily._allTransitiveDependencies,
+          screenIndex: screenIndex,
+        );
+
+  SearchItemsNotifierProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.screenIndex,
+  }) : super.internal();
+
+  final int screenIndex;
+
+  @override
+  List<BaseInfoState<Item>> runNotifierBuild(
+    covariant SearchItemsNotifier notifier,
+  ) {
+    return notifier.build(
+      screenIndex,
+    );
+  }
+
+  @override
+  Override overrideWith(SearchItemsNotifier Function() create) {
+    return ProviderOverride(
+      origin: this,
+      override: SearchItemsNotifierProvider._internal(
+        () => create()..screenIndex = screenIndex,
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        screenIndex: screenIndex,
+      ),
+    );
+  }
+
+  @override
+  NotifierProviderElement<SearchItemsNotifier, List<BaseInfoState<Item>>>
+      createElement() {
+    return _SearchItemsNotifierProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is SearchItemsNotifierProvider &&
+        other.screenIndex == screenIndex;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, screenIndex.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+mixin SearchItemsNotifierRef on NotifierProviderRef<List<BaseInfoState<Item>>> {
+  /// The parameter `screenIndex` of this provider.
+  int get screenIndex;
+}
+
+class _SearchItemsNotifierProviderElement extends NotifierProviderElement<
+    SearchItemsNotifier,
+    List<BaseInfoState<Item>>> with SearchItemsNotifierRef {
+  _SearchItemsNotifierProviderElement(super.provider);
+
+  @override
+  int get screenIndex => (origin as SearchItemsNotifierProvider).screenIndex;
+}
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
