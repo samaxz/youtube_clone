644aca9a1371fd2f951ddc0d14052109ced0de2d
samax <samax.zer@gmail.com>
2024-02-10 18:55:38 +0300

    added channel_playlist_notifier.dart

---
+++ lib/logic/notifiers/playlist/channel_playlist_notifier.dart
@@ -0,0 +1,47 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:youtube_clone/data/info/base_info.dart';
+import 'package:youtube_clone/data/info/base_info_state.dart';
+import 'package:youtube_clone/data/models/video/video_model.dart';
+import 'package:youtube_clone/logic/notifiers/providers.dart';
+
+part 'channel_playlist_notifier.g.dart';
+
+// this is used for ChannelPlaylistScreen
+@riverpod
+class ChannelPlaylistNotifier extends _$ChannelPlaylistNotifier {
+  @override
+  List<BaseInfoState<Video>> build() {
+    return [
+      const BaseInfoLoading(),
+    ];
+  }
+
+  Future<void> getPlaylistVideos(
+    String playlistId, {
+    bool isReloading = false,
+  }) async {
+    if (isReloading) {
+      state.last = const BaseInfoLoading();
+    } else {
+      state.add(const BaseInfoLoading());
+    }
+
+    state = List.from(state);
+
+    final service = ref.read(youtubeServiceP);
+    final playlistItems = await service.convertPlaylistItemsToVideos(playlistId);
+
+    state = playlistItems.fold(
+      (l) => [
+        ...state,
+        BaseInfoError(baseInfo: state.last.baseInfo, failure: l),
+      ],
+      (r) => [
+        ...state,
+        BaseInfoLoaded(
+          BaseInfo(data: r),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/logic/notifiers/playlist/channel_playlist_notifier.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'channel_playlist_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$channelPlaylistNotifierHash() =>
+    r'75f692d50648284e89f0bb094c3cab8727cc90f4';
+
+/// See also [ChannelPlaylistNotifier].
+@ProviderFor(ChannelPlaylistNotifier)
+final channelPlaylistNotifierProvider = AutoDisposeNotifierProvider<
+    ChannelPlaylistNotifier, List<BaseInfoState<Video>>>.internal(
+  ChannelPlaylistNotifier.new,
+  name: r'channelPlaylistNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$channelPlaylistNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ChannelPlaylistNotifier
+    = AutoDisposeNotifier<List<BaseInfoState<Video>>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
