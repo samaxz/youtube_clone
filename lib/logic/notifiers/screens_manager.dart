862e92409883b4c0dafbbc5394ed1e0b8a7b2977
samax <samax.zer@gmail.com>
2024-02-17 15:30:13 +0300

    changed screens_manager.dart

--- lib/logic/notifiers/screens_manager.dart
+++ lib/logic/notifiers/screens_manager.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:youtube_clone/data/custom_screen.dart';
 import 'package:youtube_clone/logic/notifiers/channel/channel_info_notifier.dart';
