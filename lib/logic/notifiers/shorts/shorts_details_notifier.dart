5d6f6ff5074a62dd6184c810c32bcf9048ee995d
samax <samax.zer@gmail.com>
2024-02-20 15:47:55 +0300

    removed comments and spaces inside shorts_details_notifier.dart

--- lib/logic/notifiers/shorts/shorts_details_notifier.dart
+++ lib/logic/notifiers/shorts/shorts_details_notifier.dart
@@ -1,24 +1,15 @@
-import 'dart:developer';
-
-import 'package:dartz/dartz.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:youtube_clone/data/info/base_info.dart';
-import 'package:youtube_clone/data/info/youtube_failure.dart';
-import 'package:youtube_clone/data/models/channel/channel_model.dart' as channel;
 import 'package:youtube_clone/data/models/video/video_details.dart';
-import 'package:youtube_clone/logic/notifiers/rating_notifier.dart';
-import 'package:youtube_clone/logic/notifiers/shorts/shorts_rating_notifier.dart';
-import 'package:youtube_clone/logic/notifiers/subscription_notifier.dart';
-import 'package:youtube_clone/logic/notifiers/video_details_notifier.dart';
-import 'package:youtube_clone/logic/services/either_extension.dart';
 import 'package:youtube_clone/logic/notifiers/providers.dart';
+import 'package:youtube_clone/logic/notifiers/shorts/shorts_rating_notifier.dart';
+import 'package:youtube_clone/logic/services/either_extension.dart';
 
 part 'shorts_details_notifier.g.dart';
 
 @riverpod
 class ShortsDetailsNotifier extends _$ShortsDetailsNotifier {
   @override
-  List<AsyncValue<VideoDetails>> build(int screenIndex) {
+  List<AsyncValue<VideoDetails>> build(String shortId) {
     return [
       const AsyncLoading(),
     ];
@@ -31,7 +22,6 @@ class ShortsDetailsNotifier extends _$ShortsDetailsNotifier {
       'Failed to load data',
       StackTrace.empty,
     );
-
     state = List.from(state);
   }
 
@@ -46,19 +36,12 @@ class ShortsDetailsNotifier extends _$ShortsDetailsNotifier {
     } else {
       state.add(const AsyncLoading());
     }
-
-    final notifier = ref.read(shortsRatingNotifierProvider(
-      videoId: videoId,
-      screenIndex: screenIndex,
-    ).notifier);
+    final notifier = ref.read(shortsRatingNotifierProvider(videoId).notifier);
     await notifier.getVideoRating();
-
     if (shouldSkip) return;
-
     final service = ref.read(youtubeServiceP);
     final channel = await service.getChannelInfoEither(channelId);
     final comments = await service.getVideoComments(videoId);
-
     // i can create a method that checks this inside service and just
     // use it here
     if (channel.rightOrDefault == null || comments.rightOrDefault == null) {
@@ -66,20 +49,17 @@ class ShortsDetailsNotifier extends _$ShortsDetailsNotifier {
         comments.leftOrDefault!,
         StackTrace.current,
       );
-
       state.last = failure;
     } else {
       final success = AsyncData(
         VideoDetails(
-          // UPD this is used for displaying channel data
+          // this is used for displaying channel data
           channelInfo: channel.rightOrDefault!,
           comments: comments.rightOrDefault!,
         ),
       );
-
       state.add(success);
     }
-
     state = List.from(state);
   }
 }
