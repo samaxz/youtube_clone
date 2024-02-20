2e79ac0f542b41e4156f131302166f074f7f5c78
samax <samax.zer@gmail.com>
2024-02-20 15:57:12 +0300

    removed comments from subs_screen.dart

--- lib/ui/screens/subs_screen.dart
+++ lib/ui/screens/subs_screen.dart
@@ -1,42 +1,16 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:youtube_clone/data/info/youtube_failure.dart';
-
 import 'package:youtube_clone/logic/notifiers/screens_manager.dart';
-import 'package:youtube_clone/logic/notifiers/subscription_notifier.dart';
-import 'package:youtube_clone/logic/services/helper_class.dart';
-import 'package:youtube_clone/logic/notifiers/providers.dart';
 import 'package:youtube_clone/ui/widgets/bodies/common_body.dart';
-import 'package:youtube_clone/ui/widgets/bodies/subs_screen_body.dart';
-import 'package:youtube_clone/ui/widgets/failure_tile.dart';
-import 'package:youtube_clone/ui/widgets/shimmers/loading_videos_screen.dart';
 
 class SubsScreen extends ConsumerWidget {
   const SubsScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // ref.listen(pushedScreensCNP, (_, state) {
-    //   if (!state.shouldPush[3]!) return;
-    //
-    //   Navigator.of(context)
-    //       .push(
-    //         MaterialPageRoute(
-    //           builder: (context) => state.screens[3]!.last.screen,
-    //         ),
-    //       )
-    //       .whenComplete(
-    //         () => state.removeLastScreen(3),
-    //       );
-    // });
-
     ref.listen(screensManagerProvider(3), (_, state) {
       final notifier = ref.read(screensManagerProvider(3).notifier);
-
       if (!notifier.shouldPush) return;
-
       Navigator.of(context)
           .push(
             MaterialPageRoute(builder: (context) => state.last.screen),
@@ -45,13 +19,6 @@ class SubsScreen extends ConsumerWidget {
             () => notifier.popScreen(),
           );
     });
-
-    // final scrollController = ref.watch(subsScrollControllerP);
-
-    return const CommonBody(
-      index: 3,
-      // scrollController: scrollController,
-      // body: const SubsScreenBody(),
-    );
+    return const CommonBody(index: 3);
   }
 }
